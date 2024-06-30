e465b6cdb24c19886e1c6d504bf42bc589ebc8a4
Justin Bottinga <JBottinga@outlook.com>
2024-06-30 16:30:24 +0200

    added picture and some cleaning up

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-PFI7WUTM.mjs";import"./chunk-EPQPQ23G.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-SEUCE3UC.mjs";import"./chunk-EPQPQ23G.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
