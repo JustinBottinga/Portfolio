a300b3c53598a6dd9bb26f1f4f9de7761abb56a3
Justin Bottinga <JBottinga@outlook.com>
2024-06-30 22:41:08 +0200

    "Update Angular app"

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WN25P3RC.mjs";import"./chunk-PHC5HC3W.mjs";import"./chunk-USI2NUIX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-LKDEY2JE.mjs";import"./chunk-PHC5HC3W.mjs";import"./chunk-USI2NUIX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
