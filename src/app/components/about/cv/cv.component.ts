a4bb387d84c8cc098aa9e293342f25cbf857bb13
Justin Bottinga <JBottinga@outlook.com>
2024-07-01 21:47:49 +0200

    small  changes

--- src/app/components/about/cv/cv.component.ts
+++ src/app/components/about/cv/cv.component.ts
@@ -8,14 +8,4 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './cv.component.html',
   styleUrl: './cv.component.css',
 })
-export class CvComponent {
-  today: Date = new Date();
-  specificDate = new Date('2024-06-27'); // Example specific date
-  passed: boolean = false;
-
-  constructor() {
-    if (this.today > this.specificDate) {
-      this.passed = true;
-    }
-  }
-}
+export class CvComponent {}
